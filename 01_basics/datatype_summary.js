df5bb4d7ea070c46b9a1966e6b3e79a555da5a95
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-20 12:26:58 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,28 @@
+// Primitive
+// 7 type :- String ,Number,BigInt,Boolean,null,undefined,symbol
+
+const score= 100
+const scoreValue=100.34
+const IsLoggedIn = false 
+const outsideTemp = null
+let userEmailId ;
+ 
+const id =Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId)
+const BigInt= 95738674879752345n
+
+// referece (Non Primitive)
+// Array, Object ,function
+
+const cars =[" BMW", "Toyota", "tata"  ]
+
+let Obj = {
+    name:"hitesh",
+    age: 22 
+}
+
+const Myfunction = function(){
+    console.log("utsav")
+}
+Myfunction();
