bfc3de9c8a5aac514cc1762034a9fc169abdde2b
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-20 05:26:30 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountID=234353
+let accountEMAIL="abcd@gmmail.com"
+var accountPassword="4886"
+accountcity="banglore"
+let accountState;
+// accountID=2 // this is not valid 
+
+accountPassword=567
+accountcity="hydrabad"
+accountEMAIL="fhgkj@gmil.com"
+console.table([accountID, accountEMAIL,accountPassword,accountcity,accountState])
+
+/* prefer to not use the var 
+because of issue in blockscope and functional scope */
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+//"use strict ";//treat all js code as newer version 
+
+//alert(3+3)// we are using nodeJs ,not browser
+
+
+
+let name= "hitesh"
+let age =18
+let isLoggedIN = false
+
+//number =>2 to 53
+//bigint
+//String=>""
+//boolean=> true/false
+// null=>undefined
+//symbol
+
+//object
+
+console.log(typeof "hitesh");
