b4f801c79bd0bc0e8bd598a47a7afaa62669e14b
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-22 12:51:17 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,24 @@
+let Array3 = ["utsav", "ravi"]
+let Array1 = ["dhruvil","vishal"]
+
+// let newArray = Array.push(Array1)
+// console.log(Array)
+
+// let allElement = Array.concat(Array1)
+// console.log(allElement)
+
+// let otherArray = [1 , 2 , 3,[4, 5 ,6 ] , 7, [8 ,9,[5]]]
+// let real_array = otherArray.flat(Infinity)
+// console.log(real_array)
+
+// console.log(Array.isArray("utsav"))
+// console.log(Array.from("utsav"))
+// console.log(Array.from({name: "utsav"}))
+
+
+let number = 100 
+let number2 = 200
+let number3 = 300
+
+console.log(Array.of(number,number2,number3))
+
