19416ef8175a38672eaf6d5c1332829b492d855b
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-23 18:46:34 +0000

    function with object

---
+++ 03_basics/01_function.js
@@ -0,0 +1,49 @@
+// function addTwonumber (number1 , number2 ){
+//     console.log(number1+number2)
+// }
+
+//  let result = addTwonumber(4 , 5)
+//  console.log("result" ,result )
+
+// function addTwonumber (number1 , number2 ){
+    
+    // let result = number1+number2
+      // return result
+//     return number1 + number2
+  
+
+// }
+
+//  let result = addTwonumber(5,6 )
+// console.log("result" ,result)
+
+function  Userloggedin(username){
+    if(username=== undefined){
+        console.log("please enter name ");
+        return
+
+    }
+    
+    return '$username{} just logged in'
+}
+// console.log();
+
+console.log( Userloggedin("utsav"))
+
+
+function cartprice(value1 ,value2,...num1){
+     return num1
+}
+
+// console.log(cartprice(100,200,300,400,500,600))
+
+let user = {
+    price : 100,
+    name : "utsav"
+}
+
+function handleobject(anyobject){
+     console.log('username ${anyobject.name} and prize ${anyobject.price}');
+    return
+}
+ handleobject(user)
