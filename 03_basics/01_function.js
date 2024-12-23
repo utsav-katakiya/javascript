799e8bfda856c8c436895833b5aec8aae967f2f3
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-23 18:52:24 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -17,33 +17,41 @@
 //  let result = addTwonumber(5,6 )
 // console.log("result" ,result)
 
-function  Userloggedin(username){
-    if(username=== undefined){
-        console.log("please enter name ");
-        return
+// function  Userloggedin(username){
+//     if(username=== undefined){
+//         console.log("please enter name ");
+//         return
 
-    }
+//     }
     
-    return '$username{} just logged in'
-}
-// console.log();
+//     return '$username{} just logged in'
+// }
+// // console.log();
 
-console.log( Userloggedin("utsav"))
+// console.log( Userloggedin("utsav"))
 
 
-function cartprice(value1 ,value2,...num1){
-     return num1
-}
+// function cartprice(value1 ,value2,...num1){
+//      return num1
+// }
 
 // console.log(cartprice(100,200,300,400,500,600))
 
-let user = {
-    price : 100,
-    name : "utsav"
+// let user = {
+//     price : 100,
+//     name : "utsav"
+// }
+
+// function handleobject(anyobject){
+//      console.log('username ${anyobject.name} and prize ${anyobject.price}');
+//     return
+// }
+//  handleobject(user)
+
+let newarray = [200,400, 500, 6000 ]
+
+function returnsecondIndexvalue(getindex){
+    return getindex[1]
 }
 
-function handleobject(anyobject){
-     console.log('username ${anyobject.name} and prize ${anyobject.price}');
-    return
-}
- handleobject(user)
+console.log(returnsecondIndexvalue(newarray));
