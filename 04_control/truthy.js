8bcab1d8a85dcd02dc4ad5143ddbfbbc7738e922
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-25 17:54:15 +0000

    control

--- 04_control/truthy.js
+++ 04_control/truthy.js
@@ -2,15 +2,28 @@
 
 // falsy false,0,-0,bigint 0n ,null ,undefined , NaN
 
-let useremail = []
-if(useremail.length === 0){
-    console.log("array empty");
+// let useremail = []
+// if(useremail.length === 0){
+//     console.log("array empty");
     
-}
+// }
 
-let empObj = {}
+// let empObj = {}
 
-if (Object.keys(empObj).length===0) {
-    console.log("object is empty");
+// if (Object.keys(empObj).length===0) {
+//     console.log("object is empty");
     
-}
+// }
+
+// // nulish coalesting operator (??) : null undifined
+// let val1 ;  
+// // val1 = 5??10
+// val1= null??20
+
+
+// ternari operator 
+
+let mark =50
+mark>70 ? console.log('yes mark less than 70') : console.log("mark grater than 50");
+
+
