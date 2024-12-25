7631e9b2c9a7bd2490287f66404beff2326f8b4b
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-25 13:12:21 +0000

    control flow

---
+++ 03_basics/iife.js
@@ -0,0 +1,23 @@
+function chai(){
+    console.log("db connected");
+    
+}
+chai();
+
+// (function chai1(){
+//     console.log("db connected");
+    
+// })();
+
+
+// (function() {
+// 	// IIFE code block
+// 	var localVar = 'This is a local variable';
+// 	console.log(localVar); // Output: This is a local variable
+// })();
+
+
+(() =>{
+    console.log("helo");
+    
+} )()
