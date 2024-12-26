9832a99527a940d9789c143215b92085a22e05a6
utsav-katakiya <130356033+utsav-katakiya@users.noreply.github.com>
2024-12-26 04:21:18 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+for(i = 1 ; i<=10 ; i++){
+    console.log(i);
+    
+    for(j = 1 ; j<=10 ; j++){
+        console.log("outer  " + i + "  inner" + j + " = "  +i*j);
+        
+    }
+}
